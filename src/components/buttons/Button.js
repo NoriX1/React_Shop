ff55a0c15fef20a11968692c000e42864ef86063
NoriX <versatility.95@mail.ru>
2020-10-18 20:51:22 +0300

    Button, Categories components

---
+++ src/components/buttons/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ outline, className, children, onClick }) => {
+  return (
+    <a href="/cart.html" className={`button ${outline && 'button-outline'} ${className}`}>
+      {children}
+    </a>
+  );
+}
+
+export default Button;
